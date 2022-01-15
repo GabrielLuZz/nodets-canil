5ed7d320e6ba95cc421f75b952118a5351336019
Gabriel Luz <gabrielluzsantosl@gmail.com>
2022-01-15 20:47:44 -0300

    A base do projeto foi criada

---
+++ src/routes/index.ts
@@ -0,0 +1,16 @@
+import { Router, Request, Response } from "express";
+import * as pageController from '../controllers/pageController';
+import * as searchController from '../controllers/searchController';
+
+const router = Router();
+
+router.get('/', pageController.home);
+router.get('/dogs', pageController.dogs);
+router.get('/cats', pageController.cats);
+router.get('fishes', pageController.fishes);
+
+router.get('/search', searchController.search);
+
+
+
+export default router;
