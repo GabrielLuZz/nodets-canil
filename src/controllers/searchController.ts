9ec6be17a97ebdf9b6564595e001e2fc908d712f
Gabriel Luz <gabrielluzsantosl@gmail.com>
2022-01-16 13:51:42 -0300

    final project 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from 'express';
+import { Pet } from '../models/pet';
+import { createMenuObject } from '../helpers/createMenuObject';
 
 export const search = (req: Request, res: Response) => {
+    let query: string = req.query.q as string;
 
+    if(!query) {
+        res.redirect('/');
+        return;
+    }
+
+    let list = Pet.getFromName(query);
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    })
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.get('/', pageController.home);
 router.get('/dogs', pageController.dogs);
 router.get('/cats', pageController.cats);
-router.get('fishes', pageController.fishes);
+router.get('/fishes', pageController.fishes);
 
 router.get('/search', searchController.search);
 
